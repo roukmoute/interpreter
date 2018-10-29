79f1bdd92beb6e7ebf0966e70d8626c36f3e97c9
Mathias STRASSER <mathias.strasser@shippeo.com>
2018-10-29 15:07:11 +0100

    Add Token

---
+++ spec/TokenSpec.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace spec\Roukmoute\Interpreter;
+
+use InvalidArgumentException;
+use PhpSpec\ObjectBehavior;
+use Roukmoute\Interpreter\Token;
+use function implode;
+use function sprintf;
+
+class TokenSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->beConstructedWith(Token::INTEGER, '0');
+
+        $this->shouldHaveType(Token::class);
+    }
+
+    function it_is_an_integer_with_the_value_0_token()
+    {
+        $this->beConstructedWith(Token::INTEGER, '0');
+
+        $this->__toString()->shouldBe('Token(INTEGER, 0)');
+    }
+
+    function it_is_an_integer_with_the_value_9_token()
+    {
+        $this->beConstructedWith(Token::INTEGER, '9');
+
+        $this->__toString()->shouldBe('Token(INTEGER, 9)');
+    }
+
+    function it_is_a_char_plus_token()
+    {
+        $this->beConstructedWith(Token::PLUS, '+');
+
+        $this->__toString()->shouldBe('Token(PLUS, +)');
+    }
+
+    function it_is_a_end_of_file_token()
+    {
+        $this->beConstructedWith(Token::EOF, null);
+
+        $this->__toString()->shouldBe('Token(EOF, null)');
+    }
+
+    function it_throws_an_exception_when_type_does_not_exist()
+    {
+        $this->beConstructedWith('BOOL', '9');
+
+        $this
+            ->shouldThrow(
+                new InvalidArgumentException(
+                    sprintf('Expected one of: "%s". Got: "BOOL"', trim(implode('", "', Token::getConstants())))
+                )
+            )
+            ->duringInstantiation()
+        ;
+    }
+}
---
+++ src/Token.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roukmoute\Interpreter;
+
+use Greg0ire\Enum\AbstractEnum;
+use Webmozart\Assert\Assert;
+use function sprintf;
+
+class Token extends AbstractEnum
+{
+    public const EOF = 'EOF';
+    public const INTEGER = 'INTEGER';
+    public const PLUS = 'PLUS';
+
+    /** @var string */
+    private $type;
+
+    /** @var string|null */
+    private $value;
+
+    /** @param string|null $value */
+    public function __construct(string $type, $value = null)
+    {
+        Assert::oneOf($type, self::getConstants());
+        Assert::nullOrString($value);
+
+        $this->type = $type;
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('Token(%s, %s)', $this->type, $this->value ?? 'null');
+    }
+}
